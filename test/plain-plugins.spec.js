78e85db7c61a8e969fe37a60b29f8a59ff9fdd89
Pelle Wessman <pelle@kodfabrik.se>
2025-08-04 00:47:45 +0200

    test: use `assert.rejects`

--- test/plain-plugins.spec.js
+++ test/plain-plugins.spec.js
@@ -36,41 +36,61 @@ describe('Plain Plugins', () => {
     });
 
     it('should throw on path traversal', async () => {
-      try {
-        await resolvePlainPlugins([
-          './traversal',
-        ], {
-          cwd: join(import.meta.url, '../test-fixtures/'),
-        });
-        assert.fail('Expected resolvePlainPlugins to fail');
-      } catch (err) {
-        assert(err instanceof Error);
-        assert.strictEqual(err.message, 'Failed to load plugin "./../index.js"');
-        assert(err.cause instanceof Error);
-        assert.match(err.cause.message, /^Path traversal detected for "\.\/\.\.\/index\.js", trying to load outside of "/);
-      }
+      await assert.rejects(
+        async () => {
+          await resolvePlainPlugins([
+            './traversal',
+          ], {
+            cwd: join(import.meta.url, '../test-fixtures/'),
+          });
+        },
+        /**
+         * @param {unknown} err
+         * @returns {true}
+         */
+        (err) => {
+          assert(err instanceof Error);
+          assert.strictEqual(err.message, 'Failed to load plugin "./../index.js"');
+
+          assert(err.cause instanceof Error);
+          assert.match(err.cause.message, /^Path traversal detected for "\.\/\.\.\/index\.js", trying to load outside of "/);
+
+          return true;
+        },
+        'Expected resolvePlainPlugins to fail'
+      );
     });
 
     it('should throw on circular dependencies', async () => {
-      try {
-        await resolvePlainPlugins([
-          './circular',
-        ], {
-          cwd: join(import.meta.url, '../test-fixtures/'),
-        });
-        assert.fail('Expected resolvePlainPlugins to fail');
-      } catch (err) {
-        assert(err instanceof Error);
-        assert.strictEqual(
-          err.message,
-          'Failed to add plugin "./circular/index.js"'.replaceAll('/', path.sep)
-        );
-        assert(err.cause instanceof Error);
-        assert.strictEqual(
-          err.cause.message,
-          'item added into group ./circular/index.js created a dependencies error'.replaceAll('/', path.sep)
-        );
-      }
+      await assert.rejects(
+        async () => {
+          await resolvePlainPlugins([
+            './circular',
+          ], {
+            cwd: join(import.meta.url, '../test-fixtures/'),
+          });
+        },
+        /**
+         * @param {unknown} err
+         * @returns {true}
+         */
+        (err) => {
+          assert(err instanceof Error);
+          assert.strictEqual(
+            err.message,
+            'Failed to add plugin "./circular/index.js"'.replaceAll('/', path.sep)
+          );
+
+          assert(err.cause instanceof Error);
+          assert.strictEqual(
+            err.cause.message,
+            'item added into group ./circular/index.js created a dependencies error'.replaceAll('/', path.sep)
+          );
+
+          return true;
+        },
+        'Expected resolvePlainPlugins to fail'
+      );
     });
 
     it('should support import.meta in place of cwd', async () => {
@@ -87,20 +107,29 @@ describe('Plain Plugins', () => {
     });
 
     it('should throw if both import.meta and cwd has been given', async () => {
-      try {
-        await resolvePlainPlugins([
-          './test-dependency',
-        ], {
-          cwd: join(import.meta.url, '../test-fixtures/'),
-          meta: {
-            url: (new URL('../test-fixtures/index.js', import.meta.url)).toString(),
-          },
-        });
-        assert.fail('Expected resolvePlainPlugins to fail');
-      } catch (err) {
-        assert(err instanceof Error);
-        assert.strictEqual(err.message, 'Can not provide both cwd and meta at once');
-      }
+      await assert.rejects(
+        async () => {
+          await resolvePlainPlugins([
+            './test-dependency',
+          ], {
+            cwd: join(import.meta.url, '../test-fixtures/'),
+            meta: {
+              url: (new URL('../test-fixtures/index.js', import.meta.url)).toString(),
+            },
+          });
+        },
+        /**
+         * @param {unknown} err
+         * @returns {true}
+         */
+        (err) => {
+          assert(err instanceof Error);
+          assert.strictEqual(err.message, 'Can not provide both cwd and meta at once');
+
+          return true;
+        },
+        'Expected resolvePlainPlugins to fail'
+      );
     });
 
     // TODO: Test prefix option
